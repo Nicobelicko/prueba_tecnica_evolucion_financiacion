ba6b3b023674dae93ef2002a164eb9e19773c159
Nicobelicko <juannicolasruizmunoz@gmail.com>
2025-03-11 19:32:17 -0500

    Defino la clase procesador_de_ventas que se encarga de toda la lógica del procesamiento de los datos de ventas. También defino las clases que necesitaré al analizar el problema y sus requisitos.

--- calculo_ventas_empresa.py
+++ calculo_ventas_empresa.py
@@ -1,3 +1,30 @@
 import pandas
 import datetime
 
+class procesador_de_ventas:
+
+    def __init__(self, ruta_archivo_ventas):
+        pass
+
+
+
+    def cargar_dataframe(self,ruta_archivo_ventas):
+        pass
+
+    def obtener_campos_requeridos(self):
+        pass
+
+    def preprocesar_datos(self):
+        pass
+
+    def convertir_fecha_datetime(self):
+        pass
+    
+    def filtrar_ventas_por_annio(self):
+        pass
+
+    def calculo_estadistico_ventas_vendedor(self):
+        pass
+
+    def calculo_estadistico_ventas_mes(self):
+        pass
