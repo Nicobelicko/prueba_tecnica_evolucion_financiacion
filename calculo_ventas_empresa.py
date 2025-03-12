ba9939939ca2153c323260b9face3942098bfada
Nicobelicko <juannicolasruizmunoz@gmail.com>
2025-03-11 20:25:05 -0500

    Implemento la lógica de la función obtener_campos_requeridos, que se encarga de quitar los campos que no son necesarios y dejar los que si lo son en el dataframe, se usa una lista para poder agregar campos en caso de necesitar añadir más.

--- calculo_ventas_empresa.py
+++ calculo_ventas_empresa.py
@@ -5,7 +5,7 @@ class procesador_de_ventas:
 
     def __init__(self, ruta_archivo_ventas):
         self.df_ventas = self.cargar_dataframe(ruta_archivo_ventas)
-
+        self.obtener_campos_requeridos()
 
 
     def cargar_dataframe(self,ruta_archivo_ventas):
@@ -15,7 +15,8 @@ class procesador_de_ventas:
             raise FileNotFoundError("El archivo de ventas no fue encontrado en la dirección ingresada.")
 
     def obtener_campos_requeridos(self):
-        pass
+        campos_requeridos = ['ID_Venta','Fecha','Producto','Cantidad','Precio_Unitario','Total_Venta','Vendedor'] # Lista para ingresar los campos que quiero en el dataframe, permite agregar y campos en caso de que el archivo excel de ventas traiga nuevso campos en el futuro.
+        self.df_ventas = self.df_ventas.loc[:,campos_requeridos]
 
     def preprocesar_datos(self):
         pass
